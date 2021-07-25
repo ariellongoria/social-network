c1d2bb1682769ea64e83435ae24b05d1291ff20d
ariellongoria <ariello97@hotmail.com>
2021-07-25 16:00:22 -0500

    user routes

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            trim: true,
+            required: "Username is required.",
+        },
+        email: {
+            type: String,
+            unique: true,
+            trim: true,
+            required: "Email is required.",
+            match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require("./routes"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network", {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => console.log(`Connected on port ${PORT}`));
